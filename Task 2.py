3192c9e42d9da9044467802ad38127463506e563
tryinluck <tryinluck@gmail.com>
2025-09-27 22:25:47 +0530

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,7 @@
+# Task 2: Create a Personalized Greeting
+
+
+first_name=input("Enter your fist name: ")
+last_name=input("Enter your last name: ")
+
+print("Hello, " +first_name+ " " +last_name+"!"+" Welcome to the Python program.")
